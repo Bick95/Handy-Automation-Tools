36e9e969ca0ac1483fb754e18c63f96794818ed9
Bick95 <daniel.bick@live.de>
2020-08-05 00:03:33 +0200

    Made script stay alive during GPIO control mode

--- Music/Online-Radio/control.py
+++ Music/Online-Radio/control.py
@@ -3,37 +3,38 @@ from gpio_controller import GPIOController
 
 
 def execute(stream, command):
-	if command in stream.commands:
-		stream.commands[command]()
-	else:
-		stream.show('Invalid command.')
+    if command in stream.commands:
+        stream.commands[command]()
+    else:
+        stream.show('Invalid command.')
 
 
 def terminal_control(stream):
 
-	command = ''
+    command = ''
 
-	while command != 'close':
-		stream.show_options()
-		command = input()
+    while command != 'close':
+        stream.show_options()
+        command = input()
 
-		execute(stream, command)
+        execute(stream, command)
 
 
 def GPIO_control(stream):
-	controller = GPIOController(stream.alternate_start_stop,
-								stream.vol_up,
-								stream.vol_down,
-								stream.station_up,
-								stream.station_down,
-								)
+    controller = GPIOController(stream.alternate_start_stop,
+                                stream.vol_up,
+                                stream.vol_down,
+                                stream.station_up,
+                                stream.station_down,
+                                )
+    input() # Workaround to keep script alive...
 
 
 def main():
-	stream = LiveStream()
-	#terminal_control(stream)
-	GPIO_control(stream)
+    stream = LiveStream()
+    #terminal_control(stream)
+    GPIO_control(stream)
 
 
 if __name__ == '__main__':
-	main()
+    main()
